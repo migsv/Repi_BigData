c36ed0da8c7017bd4d26cb417f4b9874a1665c2c
Miguel <migsveiga@gmail.com>
2025-09-23 21:09:23 -0300

    Começo da parte de BackEnd

---
+++ ChatBot-API/src/main/java/br/edu/ibmec/ChatBot_API/ChatBotApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ibmec.ChatBot_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotApiApplication.class, args);
+	}
+
+}
---
+++ ChatBot-API/src/test/java/br/edu/ibmec/ChatBot_API/ChatBotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ibmec.ChatBot_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
